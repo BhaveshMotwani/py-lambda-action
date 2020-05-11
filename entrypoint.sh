59b7f10c3add2e7dee1b767cb297580b80fb000f
bhavesh_motwani_bcg <bhavesh.motwani@bcgdv.com>
2020-05-11 14:31:16 +0800

    tweaked sam build command

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,8 +35,8 @@ deploy_lambda_function(){
 }
 
 deploy_lambda_function_sam(){
-	sam build -t "${INPUT_TEMPLATE_FILE}"
-	sam deploy --region ap-south-1 --template-file /github/workspace/template.yaml --stack-name gw-authbridge
+	sam build -t "${INPUT_TEMPLATE_FILE}" && sam package --s3-bucket "aws-sam-cli-managed-default-samclisourcebucket-w14e5f9zxk17"
+	sam deploy --region "ap-south-1" --template-file "/github/workspace/template.yaml" --stack-name "gw-authbridge" --s3-bucket "aws-sam-cli-managed-default-samclisourcebucket-w14e5f9zxk17"
 }
 
 
