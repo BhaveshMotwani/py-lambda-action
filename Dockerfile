9317b6e01ba4fdd1a2bddcb062d40554f36f0d41
bhavesh_motwani_bcg <bhavesh.motwani@bcgdv.com>
2020-05-12 10:23:45 +0800

    refactored the code to only use aws-sam-cli

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
 FROM lambci/lambda:build-python3.6
 
-RUN pip install awscli
 RUN pip install aws-sam-cli
-
 
 ADD entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,41 +1,7 @@
 #!/bin/bash
-install_zip_dependencies(){
-	echo "Installing and zipping dependencies..."
-	mkdir python
-	pip install --target=python -r "${INPUT_REQUIREMENTS_TXT}"
-	zip -r dependencies.zip ./python
-}
-
-publish_dependencies_as_layer(){
-	echo "Publishing dependencies as a layer..."
-	aws configure set region ap-south-1 --profile admin
-	local result=$(aws lambda publish-layer-version --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --zip-file fileb://dependencies.zip)
-	LAYER_VERSION=$(jq '.Version' <<< "$result")
-	rm -rf python
-	rm dependencies.zip
-}
-
-publish_function_code(){
-	echo "Deploying the code itself..."
-	zip -r code.zip . -x \*.git\*
-	aws lambda update-function-code --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --zip-file fileb://code.zip
-}
-
-update_function_layers(){
-	echo "Using the layer in the function..."
-	CONFIG=$(aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers "${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}")
-	echo $CONFIG
-}
-
-deploy_lambda_function(){
-	install_zip_dependencies
-	publish_dependencies_as_layer
-	publish_function_code
-	update_function_layers
-}
 
 deploy_lambda_function_sam(){
-	sam build -t "${INPUT_TEMPLATE_FILE}" && sam local invoke --event ./events/test_event.json
+	sam build -t "${INPUT_TEMPLATE_FILE}"
 
 	sam deploy --stack-name "gw-authbridge" --s3-bucket "aws-sam-cli-managed-default-samclisourcebucket-w14e5f9zxk17" --capabilities "CAPABILITY_IAM"
 }
